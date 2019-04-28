cb81f004291d612bee3db3f74b7bdac1a5af8778
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2019-04-28 19:02:00 +0200

    Use upstream name for pkg-config
    And simplify code a bit.

--- lmdb-sys/build.rs
+++ lmdb-sys/build.rs
@@ -5,22 +5,23 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    if pkg_config::find_library("lmdb").is_ok() {
+        return;
+    }
+
     let mut lmdb: PathBuf = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
     lmdb.push("lmdb");
     lmdb.push("libraries");
     lmdb.push("liblmdb");
-
-    if !pkg_config::find_library("liblmdb").is_ok() {
-        let target = env::var("TARGET").expect("No TARGET found");
-        let mut build = cc::Build::new();
-        if target.contains("android") {
-            build.define("ANDROID", "1");
-        }
-        build
-            .file(lmdb.join("mdb.c"))
-            .file(lmdb.join("midl.c"))
-            // https://github.com/LMDB/lmdb/blob/LMDB_0.9.21/libraries/liblmdb/Makefile#L25
-            .opt_level(2)
-            .compile("liblmdb.a")
+    let target = env::var("TARGET").expect("No TARGET found");
+    let mut build = cc::Build::new();
+    if target.contains("android") {
+        build.define("ANDROID", "1");
     }
+    build
+        .file(lmdb.join("mdb.c"))
+        .file(lmdb.join("midl.c"))
+        // https://github.com/LMDB/lmdb/blob/LMDB_0.9.21/libraries/liblmdb/Makefile#L25
+        .opt_level(2)
+        .compile("liblmdb.a")
 }
